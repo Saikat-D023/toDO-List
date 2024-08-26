af8b9b6367df5bff18df3930ce27db3f71da0c7a
Saikat-D023 <saikatdey358@gmail.com>
2024-08-26 11:02:09 +0530

    adding new script file

---
+++ script.js
@@ -0,0 +1,20 @@
+let write = document.getElementById("todo");
+        let todoList = document.getElementById("todo-list");
+
+        write.addEventListener("keyup",function(e){
+            if(e.key == "Enter"){
+                addTodo(this.value);
+                this.value = ''
+            }
+        })
+
+        function addTodo(val){
+            let list = document.createElement("li");
+            list.innerHTML =`${val}`;
+            todoList.appendChild(list);
+            console.log(val);
+
+            list.addEventListener('click',function(){
+                this.classList.toggle('done')
+            })
+        }
